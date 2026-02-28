4b8a81e8ec0fa79a49779147fbdc04c805d3b8dc
Ric <11750904+ricsatjr@users.noreply.github.com>
2026-02-28 23:53:47 +0800

    uncomment line that assigns job_id from args

--- pipeline/02_extract_features.py
+++ pipeline/02_extract_features.py
@@ -970,7 +970,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    #job_id = args.job_id if args.job_id == 'all' else int(args.job_id)
+    job_id = args.job_id if args.job_id == 'all' else int(args.job_id)
     #rasnet_dir = args.rasnet_dir if args.rasnet_dir else args.output_dir
 
     rasnet_dir = args.rasnet_dir if args.rasnet_dir else (
