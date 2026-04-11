c4d014e33badf74746187bddffe813fc4026d338
ricsatjr <ricsatjr@gmail.com>
2026-04-11 17:48:40 +0000

    fix indentation issues

--- pipeline/02_extract_features.py
+++ pipeline/02_extract_features.py
@@ -1136,7 +1136,7 @@ if __name__ == "__main__":
 
 
     if args.stage in ('rasnet', 'all'):
-	if job_id == 'all':
+        if job_id == 'all':
 	    if transient_map is not None:
 	        job_ids = sorted(transient_map['job_id'].unique())
 	    else:
